2e9c9262145fa1ad17714a564b32de16b02219c9
Arechi <aglimbenli@gmail.com>
2026-02-21 15:35:46 +0100

    Setup axum

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, http::StatusCode, response::IntoResponse, routing::get};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Food {
+    name: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/food", get(get_food));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    let _ = axum::serve(listener, app).await;
+}
+
+async fn get_food() -> impl IntoResponse {
+    let food = Food {
+        name: "Pizza".to_string(),
+    };
+
+    (StatusCode::OK, Json(food))
 }
